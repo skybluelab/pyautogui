64483c846d0a57bd0b8394861e0fde2c22611452
Clming <kimyongsung0419@gmail.com>
2020-12-13 13:03:05 +0900

    시험기간 카톡 테러

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pyautogui
+import time
 
+history = ('rlawngo','qkqh','sksms','fhqht','dz')
 print(pyautogui.position())
-pyautogui.moveTo(1750,250)
-pyautogui.click(clicks=100)
+pyautogui.moveTo(984,727)
+
+for i in range(0,5):
+    pyautogui.click()
+    pyautogui.write(history[i] , interval = 0.25)
+    pyautogui.press('enter')
+    time.sleep(1)
